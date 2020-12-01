90aeccf22ba434d208470df450bdc8322be4dc45
wei chen <weichen9083@gmail.com>
2020-12-01 20:23:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,33 @@
 # code here!
 
 class School 
-  attr_accessor :roster, :name 
   
+  attr_accessor :name, :roster 
   def initialize(name)
-    @name = name 
-    @roster ={}
+    @name = name
+    @roster = {}
+    @testsort = {}
   end 
   
-  def add_student(name,roster)
-    @roster[roster]=[name] 
+  def add_student(name, key)
+    if @roster.has_key?(key)
+      @roster[key] << name
+    else 
+      @roster[key] = [name]
+    end 
+  end 
+  
+  def grade(grade)
+    @roster[grade]
+    
+  end 
+  def sort 
+    sort_new = {}
+    @roster.each do |key,value|
+      sort_new[key] = roster[key].sort 
+    end 
+    sort_new
+
+    
   end 
 end 
